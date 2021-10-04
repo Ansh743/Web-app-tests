419edf1acda0e05398241cf9007a957160ecd74d
Anshul M <anshmaske743@gmail.com>
2021-10-03 21:47:32 -0400

    Tried adding color to paragraph

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var count = 0;
 function foo(){
-    document.getElementById('para').innerHTML += 'and adding ';
+    document.getElementsByClassName('para')[0].innerHTML += 'and adding ';
+    count++;
+    document.getElementById('btn').innerHTML = 'Clicked '+count+' time(s).';
 }
